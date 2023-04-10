e132b1c66f4136bdae7571d0dbffb9c4d73f3db7
TacTechWebdesign <richelle@tactechwebdesign.com>
2023-04-11 06:18:43 +0800

    Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    //Step 3- Chain two constructors together.
+    public class Person
+    {
+        public string Middle;
+        public string First;
+        public string Last;
+
+
+        public Person(string lastName, string firstName)
+            : this(lastName, firstName, "N/A")
+        {
+            Console.WriteLine("Constructed");
+        }
+
+        public Person(string lastName, string firstName, string middleName)
+        {
+            Middle = middleName;
+            First = firstName;
+            Last = lastName;
+            Console.WriteLine(firstName + " " + middleName + " " + lastName);
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1- Create a const variable.
+            const string name = "Richelle";
+            Console.WriteLine("Hello {0}", name);
+            Console.ReadLine();
+
+            //Step 2- Create a variable using the keyword “var.”
+            var x = 25;
+
+            Console.WriteLine("The number is {0}", x);
+
+            var person1 = new Person("Richelle", "Tac");
+            var person2 = new Person("Richelle", "Tac", "Tinam");
+
+            Console.ReadLine();
+
+        }
+    }
+}
