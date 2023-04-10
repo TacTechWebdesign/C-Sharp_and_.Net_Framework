16fb332d928926545c575f82ff359d3bbb640a9c
TacTechWebdesign <richelle@tactechwebdesign.com>
2023-04-11 06:11:56 +0800

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    //Step 1- Create a struct called Number
+    struct Number
+    {
+        //Step 1- Give it the property “Amount” and have it be of data type decimal.
+        public decimal Amount { get; set; }
+
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 2- In the Main() method, create an object of data type Number
+            //and assign an amount to it.
+            Number number = new Number();
+            number.Amount = 10;
+
+            //Step 3- Print this amount to the console.
+            Console.WriteLine("The number is " + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
