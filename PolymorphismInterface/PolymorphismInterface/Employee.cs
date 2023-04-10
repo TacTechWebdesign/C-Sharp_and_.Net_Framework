d025e41558377500e9f55bdb9eec61773ac055e4
TacTechWebdesign <richelle@tactechwebdesign.com>
2023-04-11 06:12:32 +0800

    Polymorphism Interface

---
+++ PolymorphismInterface/PolymorphismInterface/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismInterface
+{
+    //2. Have your Employee class from the previous drill
+    //inherit that interface and implement the Quit() method in any way you choose.
+
+    class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("I Quit");
+        }
+    }
+}
