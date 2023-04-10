567b1221c99c56c26f34111097514afb21706f56
TacTechWebdesign <richelle@tactechwebdesign.com>
2023-04-11 05:55:17 +0800

    AbstractClass
    The assignment asks for:
    
    Create an abstract class called Person with two properties: string firstName and string lastName.
    Give it the method SayName().
    Create another class called Employee and have it inherit from the Person class.
    Implement the SayName() method inside of the Employee class.
    Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. Call the SayName() method on the object.
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    //3. Create another class called Employee
+    //and have it inherit from the Person class.
+    //4. Implement the SayName() method inside of the Employee class.
+
+    class Employee : Person
+    {
+        public Employee(string First, string Last)
+        {
+            firstName = First;
+            lastName = Last;
+        }
+
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5. Inside the Main() method, instantiate an Employee object
+            //with firstName “Sample” and lastName “Student”.
+            //Call the SayName() method on the object.
+
+            Employee employee = new Employee("Sample", "Student");
+
+            employee.SayName();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
